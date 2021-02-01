1811bf868c552d6ee61f08948f61eda725977b0a
pavel <rembo2103@mail.ru>
2021-02-01 18:51:36 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+    $('.carousel__iner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,              
+                }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                autoplay: true,
+                arrows: false,
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                autoplay: true,
+                arrows: false,
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            
+        ]
+        
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+    
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function(){
+      $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #oreder, #thx').fadeOut();
+    });
+
+
+    $('[data-modal=thx]').on('click', function(){
+      $('.overlay, #order').fadeOut();
+      $('.overlay, #thx').fadeIn();
+    });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function(){
+        $('#order .modal__descr ').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn();
+      })
+
+    })
+
+  });
