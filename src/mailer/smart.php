454de5b68e06962d4aa1a6891358de4672152945
pavel <rembo2103@mail.ru>
2021-02-04 00:23:57 +0300

    add mailer

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'test2test2103@gmail.com';                 // Наш логин
-$mail->Password = 'nfkNL5ifz';                           // Наш пароль от ящика
+$mail->Username = '...@gmail.com';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('test2test2103@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('pavsam2103@gmail.com');     // Add a recipient
+$mail->setFrom('...@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('...@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
